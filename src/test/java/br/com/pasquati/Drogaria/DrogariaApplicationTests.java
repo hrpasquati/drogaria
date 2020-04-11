3fff47b1adb6dcd9c095deb999294758444206a6
Henrique Pasquati <hrpasquati@hotmail.com>
2020-04-11 11:00:43 -0300

    Projeto da Drogaria criado

---
+++ src/test/java/br/com/pasquati/Drogaria/DrogariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pasquati.Drogaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
