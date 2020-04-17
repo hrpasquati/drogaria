27132dab18e70092db14bc5a8ecd926fb0f75278
Henrique Pasquati <hrpasquati@hotmail.com>
2020-04-17 12:42:37 -0300

    Classes Cidade e Estado criadas. E feito o mapeamento JPA

---
+++ src/main/java/br/com/pasquati/Drogaria/domain/Cidade.java
@@ -0,0 +1,63 @@
+package br.com.pasquati.Drogaria.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Cidade implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    @JoinColumn(name = "estado_id")
+    private Estado estado;
+
+    public Cidade() {
+    }
+
+    public Cidade(Long id, String nome, Estado estados) {
+        this.id = id;
+        this.nome = nome;
+        this.estado = estados;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstados() {
+        return estado;
+    }
+
+    public void setEstados(Estado estados) {
+        this.estado = estados;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cidade cidade = (Cidade) o;
+        return Objects.equals(id, cidade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
